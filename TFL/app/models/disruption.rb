be7973cc8a4a16899fc4873fbab13ab348228dc6
Munteha Ahsan <munteha@Munteha-PC.Home>
2016-07-28 01:29:31 +0100

    Read XML file from TFL road disruptions API and associate data with Disruption BO

--- TFL/app/models/disruption.rb
+++ TFL/app/models/disruption.rb
@@ -1,4 +1,33 @@
+require 'nokogiri'
+require 'open-uri'
+
 class Disruption 
+	
+attr_accessor :id,:coordinate_log, :coordinate_lat
+
+def self.all
+   unless  Data.read.search('Disruptions').nil?
+    Data.read.search('Disruption/CauseArea/DisplayPoint/Point/coordinatesLL').map do |coordinates|
+	  new(coordinates.text)
+  	end
+   end
+end
+
+def initialize(coordinates)
+    self.coordinate_log = coordinates.split(',')[0]
+    self.coordinate_lat = coordinates.split(',')[1]
+end
+
+def persisted?
+    false
+end
+
+
+module Data
+    def self.read
+       Nokogiri::XML(open("https://data.tfl.gov.uk/tfl/syndication/feeds/tims_feed.xml"))
+    end
+end
 
 end
 
