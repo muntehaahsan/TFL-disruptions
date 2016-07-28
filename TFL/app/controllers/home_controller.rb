c99f05a637680d861876cafa55cabecdd4583b44
Munteha Ahsan <munteha@Munteha-PC.Home>
2016-07-28 10:01:05 +0100

    add disruption markers to map

--- TFL/app/controllers/home_controller.rb
+++ TFL/app/controllers/home_controller.rb
@@ -2,7 +2,11 @@ require 'nokogiri'
 require 'open-uri'
 class HomeController < ActionController::Base
   def show_map
-  	
+  	disruption_coordinates = Disruption.all
+        @hash = Gmaps4rails.build_markers(disruption_coordinates) do |disruption, marker|
+  		marker.lat disruption.coordinate_lat
+ 		marker.lng disruption.coordinate_log
+        end
   end
 end
 
